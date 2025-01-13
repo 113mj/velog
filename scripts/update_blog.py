b7387d5df761e62c535c02766162e0946e195ef4
113mj <128031749+113mj@users.noreply.github.com>
2025-01-13 17:35:13 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -1,10 +1,10 @@
-import feedparser
+mport feedparser
 import git
 import os
 
 # 벨로그 RSS 피드 URL
 # example : rss_url = 'https://api.velog.io/rss/@rimgosu'
-rss_url = 'https://api.velog.io/rss/@1113mj'
+rss_url = 'https://api.velog.io/rss/@113mj'
 
 # 깃허브 레포지토리 경로
 repo_path = '.'
